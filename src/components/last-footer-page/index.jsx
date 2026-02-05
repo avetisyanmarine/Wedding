cbceff0dc20ea99bc75b90e1d3acd86bf5b91aad
avetisyanmarine <marineavet2006@gmail.com>
2026-02-05 21:46:54 +0400

    Fixing instagram path and case sensitivity

--- src/components/last-footer-page/index.jsx
+++ src/components/last-footer-page/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "../../GlobalStyle";
 import { LastFooterPagePart, LastFooterPagePartContext } from "./styled";
 import PhoneSvg from "../../assets/vectors/Phone.svg";
-import {Insta} from "../../assets/vectors/instagram.svg";
+// import {Insta} from "../../assets/vectors/instagram.svg";
 
 export const LastFooterPage = () => {
   return (
@@ -10,7 +10,7 @@ export const LastFooterPage = () => {
         <LastFooterPagePartContext>
           <div>
             <div>
-              <img loading="lazy" src={Insta} />
+              {/* <img loading="lazy" src={Insta} /> */}
               <div>
                 <p>Փեսա</p>
                 <p>Հարս</p>
