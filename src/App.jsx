3109e95edc74647030f67677e295592e9a6ed417
avetisyanmarine <marineavet2006@gmail.com>
2026-03-14 19:16:50 +0400

    gthj

--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,20 @@ function App() {
           property="og:description"
           content="Դու հրավիրված ես մեր միջոցառմանը"
         />
-        <meta property="og:image" content="https://andranik-lilit.vercel.app/wedding.jpg" />
+        <meta
+          property="og:image"
+          content="https://andranik-lilit.vercel.app/wedding.jpg"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Հարսանյաց հրավեր" />
         <meta
           name="twitter:description"
           content="Դու հրավիրված ես մեր միջոցառմանը"
         />
-        <meta name="twitter:image" content="./assets/image/wedding.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://andranik-lilit.vercel.app/wedding.jpg"
+        />
       </Helmet>
 
       <MusicPage />
