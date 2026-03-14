89bac46d48139d2bc5029921022af10cd94fd60c
avetisyanmarine <marineavet2006@gmail.com>
2026-03-14 19:20:50 +0400

    gthj

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { ThirdPage } from "./components/third-page";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { BackToTop } from "./components/top-button";
-import { Helmet } from "react-helmet";
 import AttendanceGuests from "./components/AttendanceGuests";
 
 function App() {
@@ -18,29 +17,6 @@ function App() {
 
   return (
     <>
-      <Helmet>
-        <title>Հարսանյաց հրավեր</title>
-        <meta property="og:title" content="Հարսանյաց հրավեր" />
-        <meta
-          property="og:description"
-          content="Դու հրավիրված ես մեր միջոցառմանը"
-        />
-        <meta
-          property="og:image"
-          content="https://andranik-lilit.vercel.app/wedding.jpg"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Հարսանյաց հրավեր" />
-        <meta
-          name="twitter:description"
-          content="Դու հրավիրված ես մեր միջոցառմանը"
-        />
-        <meta
-          name="twitter:image"
-          content="https://andranik-lilit.vercel.app/wedding.jpg"
-        />
-      </Helmet>
-
       <MusicPage />
       <FirstPage />
       <SecondPage />
